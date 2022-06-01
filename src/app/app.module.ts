b67c7fee6bc844914f463eff9a70cefcf3bc985f
Angel <angelrivas0day@gmail.com>
2022-06-01 09:57:50 -0500

    Project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { DashboardLayoutComponent } from './dashboard-layout/dashboard-layout.component';
+import { LegalComponent } from './legal/legal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    DashboardLayoutComponent,
+    LegalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
